ae8499b5116c9a6b9bcceeb7cf361fce49bf64cb
Kooreis <kooreis.official@gmail.com>
2023-06-08 19:19:08 -0500

    Defined a function 'isValid' that checks if a string has valid parentheses. It uses a stack to keep track of the opening parentheses and checks for the corresponding closing parentheses.

--- solution.cpp
+++ solution.cpp
@@ -1,2 +1,13 @@
-#include <iostream>
-#include <stack>
+bool isValid(std::string s) {
+    std::stack<char> stack;
+    for (char& c : s) {
+        switch (c) {
+            case '(': case '{': case '[': stack.push(c); break;
+            case ')': if (stack.empty() || stack.top()!='(') return false; else stack.pop(); break;
+            case '}': if (stack.empty() || stack.top()!='{') return false; else stack.pop(); break;
+            case ']': if (stack.empty() || stack.top()!='[') return false; else stack.pop(); break;
+            default: ; // pass
+        }
+    }
+    return stack.empty() ;
+}
